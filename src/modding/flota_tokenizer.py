ac4b6163fe73d7929e4f21fd8d22d66494b06979
Joel Niklaus <me@joelniklaus.ch>
2022-12-30 09:39:39 -0800

    more adaptations to enable large models too

--- src/modding/flota_tokenizer.py
+++ src/modding/flota_tokenizer.py
@@ -35,7 +35,7 @@ class FlotaTokenizer:
                 attention_mask[i, :len(text)] = 1
             tensors = {'input_ids': input_ids, 'attention_mask': attention_mask}
             return tensors
-        elif self.model_path == 'roberta-base':
+        elif 'roberta-' in self.model_path:
             input_ids = self.tok.eos_token_id * torch.ones((batch_size, max_len)).long()
             attention_mask = torch.zeros((batch_size, max_len)).long()
             for i, text in enumerate(texts):
@@ -43,7 +43,7 @@ class FlotaTokenizer:
                 attention_mask[i, -len(text):] = 1
             tensors = {'input_ids': input_ids, 'attention_mask': attention_mask}
             return tensors
-        elif self.model_path == 'xlm-roberta-base':
+        elif 'xlm-roberta-' in self.model_path:
             input_ids = self.tok.pad_token_id * torch.ones((batch_size, max_len)).long()
             attention_mask = torch.zeros((batch_size, max_len)).long()
             for i, text in enumerate(texts):
@@ -69,7 +69,7 @@ class FlotaTokenizer:
                             return self.special + subword, w[:i] + l * '-' + w[i + l:], i
                         elif subword in self.vocab:
                             return subword, w[:i] + l * '-' + w[i + l:], i
-                elif self.model_path == 'roberta-base' or self.model_path == 'xlm-roberta-base':
+                elif 'roberta-' in self.model_path or 'xlm-roberta-' in self.model_path:
                     if i == 0:
                         if self.special + subword in self.vocab:
                             return self.special + subword, w[:i] + l * '-' + w[i + l:], i
@@ -99,7 +99,7 @@ class FlotaTokenizer:
                 return [w]
             elif self.special + w in self.vocab:
                 return [self.special + w]
-        elif self.model_path == 'roberta-base' or self.model_path == 'xlm-roberta-base':
+        elif 'roberta-' in self.model_path or 'xlm-roberta-' in self.model_path:
             if self.special + w in self.vocab:
                 return [self.special + w]
             elif w in self.vocab:
@@ -108,11 +108,11 @@ class FlotaTokenizer:
             flota_dict = self.get_flota_dict(w, self.k)
             return [subword for i, subword in sorted(flota_dict.items())]
         elif self.mode == 'first':
-            if self.model_path == 'roberta-base':
+            if 'roberta-' in self.model_path:
                 return self.tok.tokenize(' ' + w)[:self.k]
             return self.tok.tokenize(w)[:self.k]
         elif self.mode == 'longest':
-            if self.model_path == 'roberta-base':
+            if 'roberta-' in self.model_path:
                 subwords = enumerate(self.tok.tokenize(' ' + w))
             else:
                 subwords = enumerate(self.tok.tokenize(w))
@@ -127,8 +127,8 @@ class FlotaTokenizer:
         if 'bert-' in self.model_path:
             ids_flota = self.tok.convert_tokens_to_ids(tokens)[:self.tok.model_max_length - 2]
             return [self.tok.cls_token_id] + ids_flota + [self.tok.sep_token_id]
-        elif self.model_path == 'roberta-base':
+        elif 'roberta-' in self.model_path:
             return self.tok.convert_tokens_to_ids(tokens)[:self.tok.model_max_length]
-        elif self.model_path == 'xlm-roberta-base':
+        elif 'xlm-roberta-' in self.model_path:
             ids_flota = self.tok.convert_tokens_to_ids(tokens)[:self.tok.model_max_length - 2]
             return ids_flota + [self.tok.sep_token_id, self.tok.cls_token_id]
