9680aa485ea6103cafc1095deeabdeb19bb6bdb5
Joel Niklaus <me@joelniklaus.ch>
2023-02-03 01:20:34 -0800

    updated training script

--- scripts/train_mlm_tpu.sh
+++ scripts/train_mlm_tpu.sh
@@ -7,13 +7,27 @@ export WANDB_PROJECT="multilinguallegalpretraining"
 export TOKENIZERS_PARALLELISM=true
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 
+HF_AUTH_TOKEN='<hf_token>'
+HF_NAME=joelito
+
+HUB_MODEL_PATH=${HF_NAME}/${MODEL_NAME}
+
+
 MODEL_MAX_LENGTH=512
 MODEL_NAME=legal-french-roberta-base
 MODEL_PATH=data/plms/${MODEL_NAME}
 LANGUAGES=fr
 
-HF_AUTH_TOKEN='<hf_token>'
-HF_NAME=joelito
+STREAMING=True
+CONTINUE_TRAINING=false
+
+if [[ $CONTINUE_TRAINING == true ]]; then
+  FREEZE_MODEL_ENCODER=False
+  MODEL_NAME_OR_PATH=$HUB_MODEL_PATH
+else
+  FREEZE_MODEL_ENCODER=True
+  MODEL_NAME_OR_PATH=$MODEL_PATH
+fi
 
 # 8 TPU cores on v3-8 x batch size x accumulation steps = 512
 TOTAL_BATCH_SIZE=512
@@ -36,7 +50,8 @@ ACCUMULATION_STEPS=$(( ${TOTAL_BATCH_SIZE} / ${BATCH_SIZE} / ${TPU_CORES} ))
 # larger mlm probability because of https://arxiv.org/abs/2202.08005
 
 sudo python3 src/pretraining/xla_spawn.py --num_cores=${TPU_CORES} src/pretraining/train_mlm.py \
-    --model_name_or_path ${MODEL_PATH} \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --streaming ${STREAMING} \
     --do_train \
     --do_eval \
     --output_dir ${MODEL_PATH}-mlm \
@@ -58,11 +73,11 @@ sudo python3 src/pretraining/xla_spawn.py --num_cores=${TPU_CORES} src/pretraini
     --warmup_ratio 0.05 \
     --weight_decay 0.01 \
     --mlm_probability ${MLM_PROBABILITY} \
-    --freeze_model_encoder \
+    --freeze_model_encoder ${FREEZE_MODEL_ENCODER} \
     --max_seq_length ${MODEL_MAX_LENGTH} \
     --pad_to_max_length \
     --line_by_line \
-    --hub_model_id=${HF_NAME}/${MODEL_NAME} \
+    --hub_model_id=${HUB_MODEL_PATH} \
     --hub_strategy=checkpoint \
     --push_to_hub \
     --hub_private_repo \
